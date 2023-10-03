0f0498c9bc3c9345798d8398ec1e15833b700278
haru2016031 <83226133+haru2016031@users.noreply.github.com>
2023-10-03 14:12:50 +0900

    ジャンプ台追加

---
+++ UnityProject/Assets/Script/Game/Spring.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+	// ジャンプする力（上向きの力）を定義
+	[SerializeField] private float jumpForce = 20.0f;
+
+	/// <summary>
+	/// Colliderが他のトリガーに入った時に呼び出される
+	/// </summary>
+	/// <param name="other">当たった相手のオブジェクト</param>
+	private void OnTriggerEnter(Collider other)
+	{
+		// 当たった相手のタグがPlayerだった場合
+		if (other.gameObject.CompareTag("Player"))
+		{
+			// 当たった相手のRigidbodyコンポーネントを取得して、上向きの力を加える
+			var r = other.gameObject.GetComponent<Rigidbody>();
+			r.velocity = new Vector3(r.velocity.x, 0, r.velocity.z);
+			r.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+		}
+	}
+}
