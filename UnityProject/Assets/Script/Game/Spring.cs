eaf3a37d6ece2240bdc212637702c84a3beeb7f0
haru2016031 <83226133+haru2016031@users.noreply.github.com>
2023-10-30 15:23:24 +0900

    ステージ追加

--- UnityProject/Assets/Script/Game/Spring.cs
+++ UnityProject/Assets/Script/Game/Spring.cs
@@ -6,20 +6,28 @@ public class Spring : MonoBehaviour
 {
 	// ジャンプする力（上向きの力）を定義
 	[SerializeField] private float jumpForce = 20.0f;
-
-	/// <summary>
-	/// Colliderが他のトリガーに入った時に呼び出される
-	/// </summary>
-	/// <param name="other">当たった相手のオブジェクト</param>
-	private void OnTriggerEnter(Collider other)
+    /// <summary>
+    /// Colliderが他のトリガーに入った時に呼び出される
+    /// </summary>
+    /// <param name="other">当たった相手のオブジェクト</param>
+    private void OnTriggerEnter(Collider other)
 	{
 		// 当たった相手のタグがPlayerだった場合
 		if (other.gameObject.CompareTag("Player"))
 		{
 			// 当たった相手のRigidbodyコンポーネントを取得して、上向きの力を加える
 			var r = other.gameObject.GetComponent<Rigidbody>();
-			r.velocity = new Vector3(r.velocity.x, 0, r.velocity.z);
-			r.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+			r.velocity = Vector3.zero;//new Vector3(r.velocity.x, 0, r.velocity.z);
+			r.Sleep();
+			//Vector3 vec = transform.rotation * Vector3.up * jumpForce;
+			Vector3 vec = transform.up * jumpForce;
+			if (GetComponent<LineRenderer>())
+            {
+				var line = GetComponent<LineRenderer>();
+				line.SetPosition(0, transform.position);
+				line.SetPosition(1, transform.position + vec);
+			}
+			r.AddForce(vec, ForceMode.Impulse);
 		}
 	}
 }
