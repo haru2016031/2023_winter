17ec8e5df5849dbeea07fbf634b8841f83a635ae
samusamu02 <83334413+samusamu02@users.noreply.github.com>
2023-12-08 13:41:18 +0900

    クリアシーンのテキスト表示

--- UnityProject/Assets/Script/Clear/TextCtl.cs
+++ UnityProject/Assets/Script/Clear/TextCtl.cs
@@ -5,17 +5,46 @@ using UnityEngine.UI;
 
 public class TextCtl : MonoBehaviour
 {
-    public Text clertText;
-    public Text clerTimeText;
+    public Text clearText;
+    public Text clearTimeText;
     public GameObject player;
+    public AudioClip audioClip;
+    private Animator animator;
+    private AudioSource audioSource;
+    private bool clearTextSondFlag = false;
+    private bool clearTimeSondFlag = false;
     void Start()
     {
-        clertText.enabled = false;
-        clerTimeText.enabled = false;
+        clearText.enabled = false;
+        clearTimeText.enabled = false;
+        animator = player.GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Sway"))
+        {
+            clearText.enabled = true;
 
+            if(!clearTextSondFlag)
+            {
+                audioSource.PlayOneShot(audioClip);
+                clearTextSondFlag = true;
+            }
+            StartCoroutine(EnableClearTime(1.0f));
+        }
+    }
+
+    IEnumerator EnableClearTime(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        clearTimeText.enabled = true;
+
+        if(!clearTimeSondFlag)
+        {
+            audioSource.PlayOneShot(audioClip);
+            clearTimeSondFlag = true;
+        }
     }
 }
